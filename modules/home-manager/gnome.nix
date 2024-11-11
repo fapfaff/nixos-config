64c171ae06ddf105b5f5a0eb4ebd5ce0e6c422a3
fapfaff <fabian-pfaff@outlook.de>
2024-11-11 22:18:53 +0100

    feat: add eurkey keyboard layout for gnome

--- modules/home-manager/gnome.nix
+++ modules/home-manager/gnome.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 rec {
   home.packages = with pkgs.gnomeExtensions; [
@@ -15,6 +15,12 @@ rec {
         enabled-extensions = map (extension: extension.extensionUuid) home.packages; 
       };
       "org/gnome/mutter".experimental-features = [ "scale-monitor-framebuffer" ];
+
+      "org/gnome/desktop/input-sources" = {
+        show-all-sources = true;
+        sources = [ (lib.gvariant.mkTuple [ "xkb" "eu" ]) (lib.gvariant.mkTuple [ "xkb" "us+altgr-intl" ]) ];
+        xkb-options = [ "terminate:ctrl_alt_bksp" ];
+      };
 
       "org/gnome/shell/extensions/pop-shell" = {
 	      smart-gaps = true; 
