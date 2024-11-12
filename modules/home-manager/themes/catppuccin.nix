ddbedf3d5d4a78eb4acb3974dd9b09315d2486d3
fapfaff <fabian-pfaff@outlook.de>
2024-11-13 00:10:27 +0100

    feat: add vscode catppuccin theme

--- modules/home-manager/themes/catppuccin.nix
+++ modules/home-manager/themes/catppuccin.nix
@@ -6,4 +6,16 @@
   gtk.catppuccin.gnomeShellTheme = true;
 
   programs.starship.catppuccin.enable = true;
+
+  programs.vscode = {
+    extensions = with pkgs.vscode-extensions; [
+      catppuccin.catppuccin-vsc
+      catppuccin.catppuccin-vsc-icons
+    ];
+
+    userSettings = {
+      workbench.colorTheme =  "Catppuccin Mocha";
+      workbench.iconTheme = "catppuccin-mocha";   
+    };
+  };
 }
