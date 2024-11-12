6e0e24ac9421189fecff096697853a35e745f5a0
fapfaff <fabian-pfaff@outlook.de>
2024-11-12 12:33:26 +0100

    feat: add catppuccin flake

--- flake.nix
+++ flake.nix
@@ -8,19 +8,28 @@
       url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    catppuccin.url = "github:catppuccin/nix";
   };
 
   outputs = { self, nixpkgs, ... }@inputs: {
     nixosConfigurations.default = nixpkgs.lib.nixosSystem {
       specialArgs = {inherit inputs;};
       modules = [
+        inputs.home-manager.nixosModules.home-manager
+	inputs.catppuccin.nixosModules.catppuccin
+
         hosts/default/configuration.nix
-        inputs.home-manager.nixosModules.default
 	{
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
-          home-manager.users.fp = import hosts/default/home.nix;
-        }
+          home-manager.users.fp = {
+	    imports = [
+	      hosts/default/home.nix
+              inputs.catppuccin.homeManagerModules.catppuccin
+	    ];
+          };
+	}
       ];
     };
   };
