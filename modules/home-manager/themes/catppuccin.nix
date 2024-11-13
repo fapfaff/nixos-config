3a667a911e84687c8f10a0947ac143884756310e
fapfaff <fabian-pfaff@outlook.de>
2024-11-13 10:36:37 +0100

    feat: add catppuccin icons

--- modules/home-manager/themes/catppuccin.nix
+++ modules/home-manager/themes/catppuccin.nix
@@ -5,6 +5,9 @@
   gtk.catppuccin.enable = true;
   gtk.catppuccin.gnomeShellTheme = true;
 
+  gtk.catppuccin.icon.enable = true;
+  gtk.catppuccin.icon.accent = "lavender";
+
   programs.starship.catppuccin.enable = true;
 
   programs.vscode = {
