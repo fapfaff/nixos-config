07331d97772357ef58a04a69edee3280e6bb5b25
fapfaff <fabian-pfaff@outlook.de>
2024-11-23 11:16:41 +0100

    feat: add intellij

--- modules/home-manager/dev.nix
+++ modules/home-manager/dev.nix
@@ -6,7 +6,11 @@
     ./dev/git.nix
     ./dev/python.nix
     ./dev/frontend.nix
-  ]
+  ];
+
+  home.packages = with pkgs; [
+        (jetbrains.plugins.addPlugins jetbrains.idea-ultimate ["github-copilot"])   
+  ];
 
   programs.vscode = {
     enable = true;
