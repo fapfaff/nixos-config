a118d0fbcbfb878939275b0d524f9a0f4f8cfb3b
fapfaff <fabian-pfaff@outlook.de>
2024-11-25 07:43:38 +0100

    feat: add thunderbird catppuccin  theme

--- modules/home-manager/themes/catppuccin.nix
+++ modules/home-manager/themes/catppuccin.nix
@@ -1,5 +1,10 @@
 { config, pkgs, ... }:
 let
+  thunderbird = {
+    theme = "mocha";
+    accent = "blue";
+  };
+
   wallpaper = pkgs.fetchurl {
     url = "https://raw.githubusercontent.com/zhichaoh/catppuccin-wallpapers/main/misc/windows-error.jpg";
     sha256 = "62bdf59ca592d722e4c6ce86c5843a8c7c0a809f9bf62a8f7196c1634b2e18b8";
@@ -39,6 +44,18 @@ in
       picture-uri = "file://" + wallpaper;
       picture-uri-dark = "file://" + wallpaper;
       picture-options = "wallpaper";
+    };
+  };
+
+  programs.thunderbird.package = pkgs.thunderbird.override {
+    extraPolicies = {
+      ExtensionSettings = {
+        "{f6d05f0c-39a8-5c4d-96dd-4852202a8244}" = {
+          installation_mode = "normal_installed";
+          install_url = "https://github.com/catppuccin/thunderbird/raw/refs/heads/main/themes/${thunderbird.theme}/${thunderbird.theme}-${thunderbird.accent}.xpi";
+          updates_enabled = true;
+        };
+      };
     };
   };
 }
