555da3518b092af92f7d34e46e1d90e7c706f1f3
fapfaff <fabian-pfaff@outlook.de>
2026-05-31 20:38:28 +0200

    fix: docker and captive portal collision

--- modules/nixos/dev.nix
+++ modules/nixos/dev.nix
@@ -1,10 +1,27 @@
 { config, pkgs, ... }:
 
 {
-  virtualisation.docker.enable = true;
+  virtualisation.docker = {
+    enable = true;
+
+    daemon.settings = {
+      default-address-pools = [
+        {
+          base = "10.200.0.0/16";
+          size = 24;
+        }
+        {
+          base = "10.201.0.0/16";
+          size = 24;
+        }
+      ];
+    };
+  };
+
   users.users.fp.extraGroups = [ "docker" ];
 
   environment.systemPackages = with pkgs; [
     nixfmt-rfc-style
+    glib
   ];
 }
